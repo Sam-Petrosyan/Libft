0e476d8452309771b03c98be286aa4d47f980bbb
Samvel Petrosyan <spetrosy@c2r9s3.42yerevan.am>
2022-04-16 09:52:55 +0400

    Finaly finished Libft

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: spetrosy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 16:20:27 by spetrosy          #+#    #+#             */
-/*   Updated: 2022/04/14 19:23:00 by spetrosy         ###   ########.fr       */
+/*   Updated: 2022/04/16 09:49:33 by spetrosy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	d = dest;
 	s = src;
 	if (d < s)
-		ft_memspy(dest, src, n);
+		ft_memcpy(dest, src, n);
 	else
 	{
 		while (n--)
